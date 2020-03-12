3017ad55a76b8abf5be232e85200e2cc1d7f7ff0
Cassandra Dew <cpdew2@gmail.com>
2020-03-12 19:03:22 -0400

    mvp

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css'; 
-import { Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 
-
- return (
-   <div className="App">
-      <nav>
-        <div className="nav-links">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
-        </div>
-      </nav>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-    </div>
-  );
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
