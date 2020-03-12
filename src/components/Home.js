4218b0d5974bcfffd30d038e17646b6ab7809959
Cassandra Dew <cpdew2@gmail.com>
2020-03-11 20:50:09 -0400

    import Router Route and Link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const Home = () => <h1>Home</h1>;
+const Home = () =>
+
+<h1>Home</h1>;
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css'; 
-import { Router } from 'react-router-dom';
+import { Router, Route, Link } from 'react-router-dom';
 
+
+ return (
+   <div className="App">
+      <nav>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </nav>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  );
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
